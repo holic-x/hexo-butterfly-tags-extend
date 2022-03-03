8a095b0a9a1e1096d9ca32f4713b5a8c06e703c3
holic-x <43883789+holic-x@users.noreply.github.com>
2022-03-04 01:46:18 +0800

    自定义标签插件
    自定义标签插件：引入文本、chart等相关插件，调整部分细节

--- README.md
+++ README.md
@@ -2,9 +2,7 @@
 
 ## About | 关于
 
-​	基于hexo-butterfly的基础上扩展标签插件
-
-
+​	基于hexo-butterfly的基础上扩展自定义标签插件，标签插件开发基本内容参考hexo插件库，针对项目和日常应用引入自定义的外置标签，对部分标签内容做了相应的调整和优化
 
 
 
@@ -18,11 +16,31 @@ npm -i hexo-butterfly-tags-extend
 
 ## 使用手册参考
 
-
+​	针对现有标签库的应用参考文章，会结合插件库开发进行调整
 
 
 
 ## 更新说明
+
+> 202109
+
+👻add：bilibili-视频引入（调整参数配置项）
+
+👻add：githubRepo-github仓库卡片展示（将css、js引用抽离，调整部分样式）
+
+👻add：图表类型-highcharts、chart、echarts多样图表展示（引入不同类型的图表）
+
+👻add：asciinema-linux终端记录&回放组件嵌入（图片、视频）
+
+
+
+> 202110
+
+💣fix：修复githubRepo样式重复引用以及和其他组件冲突问题
+
+💣fix：调整样式引用和标签注册，将样式定义和标签渲染函数注册进行分离
+
+💣fix：tag标签迭代，重新优化代码结构，调整配置项
 
 
 
--- index.js
+++ index.js
@@ -1,19 +1,131 @@
-// ----------- 插件注册 --------------------
+// ----------- 标签注册 --------------------
+const defaultTagList = [
+  'showText','bilibili','githubRepo',
+  'highcharts','chart','echarts',
+  'asciinema','poem'
+]
 
-/**
- * bilibili插件注册
- */
-hexo.extend.tag.register('showText', require('./lib/scripts/tag/showText'), { ends: false })
-hexo.extend.tag.register('bilibili', 
-  function(args){
-    // 将hexo传入
-    return require('./lib/scripts/tag/bilibili').addBilibiliVideo(hexo,args);
+// 参数配置校验
+const config = hexo.config.tags_extend || hexo.theme.config.tags_extend;
+// 校验配置开关
+if(!config || !config.enable){
+  return ;
+}
+
+// 配置项控制
+const urlFor = require('hexo-util').url_for.bind(hexo)
+const tagConfig = {
+  tags_extend_css: config.CDN.tags_extend_css ? urlFor(config.CDN.tags_extend_css) : 'https://pic-bed-1305292171.cos.ap-shanghai.myqcloud.com/tags_extend.css'
+}
+
+// 装载样式,注入css资源
+const css_text = `<link rel="stylesheet" href=${tagConfig.tags_extend_css} media="defer" onload="this.media='all'">`
+hexo.extend.injector.register('head_end', css_text, "default");
+
+
+// 校验所需参数配置列表
+const enableTagList = config.tagList?defaultTagList:config.tagList;// 启动对应参数配置列表
+for(var i=0;i<enableTagList.length;i++){
+  switch (enableTagList[i]) {
+    // showText标签注册
+    case 'showText':{
+      hexo.extend.tag.register('showText', require('./lib/scripts/text/showText'), { ends: false })
+      break;
+    }
+    
+    //  bilibili标签注册
+    case 'bilibili':{
+      hexo.extend.tag.register('bilibili', 
+      function(args){
+        // 将hexo传入
+        return require('./lib/scripts/av/bilibili').addBilibiliVideo(hexo,args);
+      }
+      , { ends: false })
+      break;
+    }
+       
+    // githubRepo标签注册
+    case 'githubRepo':{
+      // 注册标签
+      hexo.extend.tag.register('githubRepo', async function (args, content) {
+        return require('./lib/scripts/tag/githubRepo').githubRepoRender(args, content);
+      }, { async: true });
+
+      break;
+    }
+
+    // highcharts标签注册
+    case 'highcharts':{
+      hexo.extend.tag.register('highcharts',function (args, content) {
+        // 其中content对应的是图标的options配置
+        return require('./lib/scripts/chart/highcharts').highchartsRender(args, content);
+      },{
+        async: true,
+        ends: true
+      })
+      break;
+    }
+
+
+    // chart标签注册
+    case 'chart':{
+      hexo.extend.tag.register('chart',function (args, content) {
+        // 其中content对应的是图标的options配置
+        return require('./lib/scripts/chart/chart.js').chartRender(args, content);
+      },{
+        async: true,
+        ends: true
+      })
+      break;
+    }
+    
+     // echarts标签注册
+     case 'echarts':{
+      hexo.extend.tag.register('echarts',function (args, content) {
+        // 其中content对应的是图标的options配置
+        return require('./lib/scripts/chart/echarts.js').echartsRender(args, content);
+      },{
+        async: true,
+        ends: true
+      })
+      break;
+    }
+
+    // asciinema标签注册
+    case 'asciinema':{
+      hexo.extend.tag.register('asciinema',function (args, content) {
+        // 其中content对应的是图标的options配置
+        return require('./lib/scripts/tag/asciinema.js').asciinemaRender(args, content);
+      },{
+        ends: false
+      })
+      break;
+    }
+
+    // poem标签注册
+    /*
+    case 'poem':{
+      hexo.extend.tag.register('poem',function (args, content) {
+        // 其中content对应的是图标的options配置
+        return require('./lib/scripts/text/poem.js').poemRender(hexo,args, content);
+      },{
+        ends: true
+      })
+      break;
+    }
+    */
+
+
+    // 其他配置
+    default:{
+      console.log("未知的标签类型定义"+enableTagList[i]);
+    }
   }
-, { ends: false })
 
-/**
- * githubRepo插件注册
- */
-hexo.extend.tag.register('githubRepo', async function (args, content) {
-  return require('./lib/scripts/tag/githubRepo').genGithubRepoTag(args, content);
-}, { async: true });
+}
+
+
+
+ 
+
+ 
